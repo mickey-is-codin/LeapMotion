30cfcf8004557c87ae571e5099b3153aecf8cc11
smit2300 <smithmick14@gmail.com>
2017-10-24 13:37:43 -0400

    Added python graphing functionality

---
+++ Python/MatPlotLibExp.py
@@ -0,0 +1,17 @@
+import matplotlib.pyplot as plt
+
+x = [1,2,3]
+y = [5,7,4]
+
+x2 = [1,2,3]
+y2 = [10,14,12]
+
+plt.plot(x,y, label = 'First Line')
+plt.plot(x2,y2, label = 'Second Line')
+plt.xlabel('X axis')
+plt.ylabel('Y axis')
+plt.legend()
+
+plt.title('Graph')
+
+plt.show()
--- Python/experimenting.py
+++ Python/experimenting.py
@@ -1,7 +1,11 @@
 import Leap, sys, thread, time
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
+
+x = []
+handHeight = []
 
 class LeapMotionListener(Leap.Listener):
     finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
@@ -28,64 +32,50 @@ class LeapMotionListener(Leap.Listener):
     def on_frame(self, controller):
         frame = controller.frame()
 
-        print "Frame ID: " + str(frame.id) \
+        positionRight = 0
+
+        """print "Frame ID: " + str(frame.id) \
             + " Timestamp: " + str(frame.timestamp) \
             + " Number of Hands: " + str(len(frame.hands)) \
             + " Number of Fingers: " + str(len(frame.fingers)) \
             + " Number of Tools: " + str(len(frame.tools)) \
-            + " Number of Gestures: " + str(len(frame.gestures()))
+            + " Number of Gestures: " + str(len(frame.gestures()))"""
 
         for hand in frame.hands:
             if hand.is_left:
                 handType = "Left Hand"
-                
-                #leftThumb = hand.fingers[0]
-                #leftIndex = hand.fingers[0]
-                #leftMiddle = hand.fingers[0]
-                #leftRing = hand.fingers[0]
-                #leftPinky = hand.fingers[0]
+
+                leftThumb = hand.fingers[0]
+                leftIndex = hand.fingers[0]
+                leftMiddle = hand.fingers[0]
+                leftRing = hand.fingers[0]
+                leftPinky = hand.fingers[0]
+
             else:
                 handType = "Right Hand"
 
-                #rightThumb = hand.fingers[0]
-                #rightIndex = hand.fingers[0]
-                #rightMiddle = hand.fingers[0]
-                #rightRing = hand.fingers[0]
-                #rightPinky = hand.fingers[0]
+                rightThumb = hand.fingers[0]
+                rightIndex = hand.fingers[0]
+                rightMiddle = hand.fingers[0]
+                rightRing = hand.fingers[0]
+                rightPinky = hand.fingers[0]
 
-            print handType + " Hand ID: " + str(hand.id) + " Palm Position: " + str(hand.palm_position)
+                positionRight = hand.palm_position.y
+
+                handHeight.append(positionRight)
+
+            #print handType + " Hand ID: " + str(hand.id) + " Palm Position: " + str(hand.palm_position)
 
             normal = hand.palm_normal
             direction = hand.direction
 
-            #Plotting the palm position
-            plt.axis([0, 10, 0, 1])
-            plt.ion()
+            #print "Pitch: " + str(direction.pitch * Leap.RAD_TO_DEG)
+            #print "Roll: " + str(direction.roll * Leap.RAD_TO_DEG)
+            #print "Yaw: " + str(direction.yaw * Leap.RAD_TO_DEG)
 
-            print "Pitch: " + str(direction.pitch * Leap.RAD_TO_DEG)
-            print "Roll: " + str(direction.roll * Leap.RAD_TO_DEG)
-            print "Yaw: " + str(direction.yaw * Leap.RAD_TO_DEG)
-
-            arm = hand.arm
-            print "Arm Direction: " + str(arm.direction)
-            print "Wrist Position: " + str(arm.wrist_position)
-            print "Elbow Position: " + str(arm.elbow_position)
-
-            for finger in hand.fingers:
-                print "Type: " + self.finger_names[finger.type()] + " ID: " + str(finger.id) + " Length (mm): " + str(finger.length) + " Width (mm): " + str(finger.width)
-
-                for b in range(0, 4):
-                    bone = finger.bone(b)
-                    print "Bone: " + self.bone_names[bone.type] + " Start: " + str(bone.prev_joint) + " End: " + str(bone.next_joint) + " Direction: " + str(bone.direction)
-
-    def plot(): #Eventually accept some hand parameters as arguments
-        for i in range(10):
-                y = np.random.random()
-                plt.scatter(i,y)
-                plt.pause(0.05)
-
-            while True:
-                plt.pause(0.05)
+        #x.append(frame.time)
+        #plt.plot(x,handHeight)
+        print "Hand Height (mm): " + "%.0f" % positionRight
 
     
 def main():
@@ -101,6 +91,7 @@ def main():
         pass
     finally:
         controller.remove_listener(listener)
+
         
 if __name__ == "__main__":
     main()
